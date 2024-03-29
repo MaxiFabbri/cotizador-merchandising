59ba0b7f964e28f0ce2ebf960128aa41d4cb8206
Maxi Fabbri <maxifabbri1972@gmail.com>
2024-03-28 21:57:32 -0300

    con-Local-storage-funcionando

--- cotizador-merch/js/main.js
+++ cotizador-merch/js/main.js
@@ -1,72 +1,113 @@
 const impuestos = 5
-const cambioDolar = parseFloat(868.5)
-const Cotizaciones = []
+const hoy = new Date()
+let cambioDolar = parseFloat(868.5)
+let cotizaciones = []
+const fechaCorta = fecha => fecha.getDate()+"-"+(fecha.getMonth()+1)+"-"+fecha.getFullYear()
 
+
+// defino la tabla utilidades segun el costo total
+const escalasUtilidades = [
+    {hasta: 408, utilidad: 30 , minimo: 82},
+    {hasta: 1429, utilidad: 28 , minimo: 188},
+    {hasta: 2041, utilidad: 25 , minimo: 597},
+    {hasta: 4082, utilidad: 22 , minimo: 729},
+    {hasta: 9999999999, utilidad: 20 , minimo: 1230}
+]
+
+// Creo la Clase Cotizaciones con todos los datos relevantes
 class Cotizacion {
-    static id = 0
-    constructor (cliente, proveedor, producto, logo, cantidad, costoFijo, otrosCostos, precioUnitario) {
+    static id=0
+    constructor (fecha, cambioUsado, cliente, proveedor, producto, logo, cantidad, costoUnitario, costoFijo, otrosCostos, precioUnitario) {
         this.id = ++Cotizacion.id
+        this.fecha = fecha,
+        this.cambioUsado = cambioUsado,
         this.cliente = cliente,
         this.proveedor = proveedor,
         this.producto = producto,
         this.logo = logo,
         this.cantidad = cantidad,
+        this.costoUnitario = costoUnitario,
         this.costoFijo = costoFijo,
         this.otrosCostos = otrosCostos,
-        this.precioUnitario = precioUnitario
+        this.precioUnitario = precioUnitario     
     }
 }
 
-// Menu de inicio
-let menu = parseInt(prompt("Bienvenido! Elija:\n\n1 Para ver las cotizaciones,\n2 Para cargar una nueva cotización,\n3 Para salir"))
-
-while(menu !==3){
-    switch (menu) {
-        case 1:
-            verCotizaciones()
-            break
-        case 2:
-            ingresoDeDatos()
-            break 
-        default:
-            alert("Opcion incorrecta")
-            break
-    }
-    menu = parseInt(prompt("Bienvenido! Elija:\n\n1 Para ver las cotizaciones,\n2 Para cargar una nueva cotización,\n3 Para salir"))
+// funcion que agrega de a 1 cotizacion a la tabla HTML en forma de <tr> Table Row
+let mostrarCotizaciones = document.getElementById("cuerpoDeTabla")
+function agregarCotizATabla(cotiz) {
+    let fila = document.createElement("tr")
+    fila.innerHTML = `<td>${cotiz.cambioUsado}</td>
+                      <td>${cotiz.fecha}</td>
+                      <td>${cotiz.cliente}</td>
+                      <td>${cotiz.proveedor}</td>
+                      <td>${cotiz.producto}</td>
+                      <td>${cotiz.logo}</td>
+                      <td>${(cotiz.cantidad)}</td>
+                      <td>$ ${(cotiz.costoUnitario*cotiz.cambioUsado).toFixed(2)}</td>
+                      <td>$ ${(cotiz.costoFijo*cotiz.cambioUsado).toFixed(2)}</td>
+                      <td>$ ${(cotiz.otrosCostos*cotiz.cambioUsado).toFixed(2)}</td>
+                      <td>$ ${(cotiz.precioUnitario*cotiz.cambioUsado).toFixed(2)}</td>`
+    mostrarCotizaciones.appendChild(fila)
 }
 
+// Reviso el Local Storage y recupero los datos y los pongo en el Array Cotizaciones
+let cotizacionesLS = localStorage.getItem('cotizaciones')
+if (cotizacionesLS) {
+    let cotizacionesProv =[]
+    cotizacionesProv = JSON.parse(cotizacionesLS)
+    cotizacionesProv.forEach(cotizEl => {
+        nuevaCotizacion(cotizEl.fecha, cotizEl.cambioUsado, cotizEl.cliente, cotizEl.proveedor, cotizEl.producto, cotizEl.logo, cotizEl.cantidad, cotizEl.costoUnitario, cotizEl.costoFijo, cotizEl.otrosCostos, cotizEl.precioUnitario)
+    });
+} else {
+    cotizaciones = []
+}
 
-function ingresoDeDatos () {
-    // Le pido al usuario los datos para el calculo del precio y los paso a Dolares
-    let cliente = prompt('Ingrese Cliente:')
-    let proveedor = prompt('Ingrese Proveedor:')
-    let producto = prompt('Ingrese Producto:')
-    let logo = prompt('Ingrese Logo:')
-    let cantidad = parseInt(prompt('Cantidad:'))
-    let costoUnitario = (parseFloat(prompt('Costo Unitario:'))/cambioDolar)
-    let costoFijo = (parseFloat(prompt('Costo Fijo:'))/cambioDolar)
-    let otrosCostos = (parseFloat(prompt('Otros Costos:'))/cambioDolar)
+// Creo el evento sobre el boton cotizar
+let cotizar = document.getElementById("botonCotizar")
+cotizar.onclick = () => { inputDatos()}
+
+// Input de Datos por HTML
+function inputDatos() {
+    let cambioUsado = cambioDolar
+    let fecha = fechaCorta(hoy)
+    let cliente = (document.getElementById("inCliente")).value
+    let proveedor = (document.getElementById("inProveedor")).value
+    let producto = (document.getElementById("inProducto")).value
+    let logo = (document.getElementById("inLogo")).value
+    let cantidad = (document.getElementById("inCantidad")).value
+    let costoUnitario = ((document.getElementById("inCostoUnitario")).value/cambioUsado)
+    let costoFijo = ((document.getElementById("inCostoFijo")).value/cambioUsado)
+    let otrosCostos = ((document.getElementById("inOtrosCostos")).value/cambioUsado)
+    
+    // llamo a la funcion que Calcula el precio calculoPrecio
     let precioUnitario = calculoPrecio(cantidad, costoUnitario, costoFijo, otrosCostos)
 
-    console.log('Final - El precio unitario de venta es: '+(precioUnitario*cambioDolar).toFixed(1)+' y el precio total es: '+ (precioUnitario*cambioDolar*cantidad).toFixed(2))
+    // Muestro el precio en la Web
+    document.getElementById("outPrecioUnitario").innerText = (precioUnitario*cambioUsado).toFixed(2)
+    
+    nuevaCotizacion (fecha, cambioUsado, cliente, proveedor, producto, logo, cantidad, costoUnitario, costoFijo, otrosCostos, precioUnitario)
 
-    // Creo una nueva cotización y la cargo en un array
-    const cotizacion = new Cotizacion(cliente, proveedor, producto, logo, cantidad, costoFijo, otrosCostos, precioUnitario)
-    console.log(cotizacion)
-    Cotizaciones.push(cotizacion)
+    const cotizacionesEnJson = JSON.stringify(cotizaciones)
+    localStorage.setItem('cotizaciones',cotizacionesEnJson)
 }
 
+// Creo una nueva cotización instancia de la Clase "Cotizacion" y la cargo en un array "Cotizaciones" y la muestro en la tabla
+function nuevaCotizacion (fecha, cambioUsado, cliente, proveedor, producto, logo, cantidad, costoUnitario, costoFijo, otrosCostos, precioUnitario) {
+    const cotizacion = new Cotizacion(fecha, cambioUsado, cliente, proveedor, producto, logo, cantidad, costoUnitario, costoFijo, otrosCostos, precioUnitario)
+    cotizaciones.push(cotizacion)
+    agregarCotizATabla(cotizacion)
+}
 
-
+// Funcion que calcula el precio en base a los datos
 function calculoPrecio(cantidad, costoUnitario, costoFijo, otrosCostos){
     // calculo el costo Neto y Total
     let costoNeto = ((cantidad*costoUnitario)+costoFijo)
     let costoTotal = (costoNeto+otrosCostos)
-    
-    // llamo a la funcion que calcula la utilidad deseada, dependiendo del importe total
-    const utilidadDeseada = calculaUtilidadDeseada(costoNeto)
-    let porcentajeUtilidad = parseInt(utilidadDeseada.porcentajeUti)
-    let utilidadMinima = parseInt(utilidadDeseada.utilidadMin)
+    // Busco en la tabla de escalas de Utilidad, la correspondiente a este costo Neto
+    const utilidadDeseada = escalasUtilidades.find(escala => costoNeto < escala.hasta)
+    let porcentajeUtilidad = parseInt(utilidadDeseada.utilidad)
+    let utilidadMinima = parseInt(utilidadDeseada.minimo)
 
     // calculo el precio Unitario Probable por rentabilidad de %
     let precioUnitarioPorcentaje = (costoTotal/(1-((porcentajeUtilidad+impuestos)/100)))/cantidad
@@ -82,39 +123,7 @@ function calculoPrecio(cantidad, costoUnitario, costoFijo, otrosCostos){
     } else {
         var precioUnitario = (costoTotal + impuestoEstimado + utilidadMinima)/cantidad
     }
-    console.log('El precio unitario de venta es: '+(precioUnitario*cambioDolar).toFixed(1)+' y el precio total es: '+ (precioUnitario*cambioDolar*cantidad).toFixed(2))
-    return precioUnitario   
-}
-
-
-
-// defino la funcion que calcula la utilidad deseada
-function calculaUtilidadDeseada (netCost) {    
-    // defino la tabla utilidades segun el costo total
-    const escalasUtilidades = [
-        {hasta: 408, utilidad: 30 , minimo: 82},
-        {hasta: 1429, utilidad: 28 , minimo: 188},
-        {hasta: 2041, utilidad: 25 , minimo: 597},
-        {hasta: 4082, utilidad: 22 , minimo: 729},
-        {hasta: 9999999999, utilidad: 20 , minimo: 1230}
-    ]
-    // busco en la tabla la escala segun el costo total
-    for (const itemUtilidad of escalasUtilidades){
-        if (netCost<itemUtilidad.hasta){
-            let utilidadDeseada = {porcentajeUti: itemUtilidad.utilidad, utilidadMin: itemUtilidad.minimo}
-            return utilidadDeseada
-        } 
-    }
-}
-
-// funcion para ver las cotizaciones ya realizadas
-function verCotizaciones () {
-    if (Cotizaciones.length === 0) {
-        console.log("No hay cotizaciones realizadas")
-    } else {
-        for (const cotizacion of Cotizaciones) {
-            console.log(cotizacion)
-        }
-    }
+    // console.log('El precio unitario de venta es: '+(precioUnitario*cambioUsado).toFixed(1)+' y el precio total es: '+ (precioUnitario*cambioUsado*cantidad).toFixed(2))
+    return precioUnitario 
 }
 
